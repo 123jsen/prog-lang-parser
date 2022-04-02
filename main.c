6dc49e4729235ee0b7c2df3f2a19c87e431f86d6
SenYanHo <123sendodo@gmail.com>
2022-04-02 20:15:12 +0800

    finished parsing pring

--- main.c
+++ main.c
@@ -4,48 +4,126 @@
 #include <getopt.h>
 #include <string.h>
 
+int verbose = 0;
+
 void printHelp() {
-	printf("This program transpiles sendodoLang to C.\n");
+	printf("This program transpiles sendodoLang (suggested file format: .dldl) to C.\n");
+}
+
+// returns the index of the first matched word
+int match(char * bigStr, char * subStr) {
+	int count = 0;
+	int currentPosCnt = -1;
+	int lenSubStr = strlen(subStr);
+	int lenBigStr = strlen(bigStr);
+
+	for (int i = 0; i < lenBigStr; i++)
+	{
+		if (count == 0)
+		{
+			if (bigStr[i] == subStr[0])
+			{
+				currentPosCnt = i;
+				count ++;
+			}
+		}
+		else 
+		{
+			count ++;
+			if (bigStr[i] != subStr[count])
+			{
+				count = 0;
+			}
+			if (count == (lenSubStr - 1))
+			{
+				return currentPosCnt;
+			}
+		}
+	}
+
+	return currentPosCnt;
 }
 
 void outputCProg(char * filename, char * outputname) {
 	// open file
-	FILE *fptr;
+	FILE *inPtr = fopen(filename, "r");
 
-	printf("#include <stdio.h>\n#include <stdlib.h>\n");
-	printf("int main() {\n");
+	if (inPtr == NULL)
+	{
+		printf("Error: could not open file %s\n", filename);
+		return;
+	}
 
-	printf("}\n");
+	FILE *outPtr = fopen(outputname, "w");
+	
+	if (outPtr == NULL)
+	{
+		printf("Error: could not open file %s\n", outputname);
+		return;
+	}
+
+	if (verbose) printf("success opening both files\n");
+
+	fprintf(outPtr, "#include <stdio.h>\n#include <stdlib.h>\n");
+	fprintf(outPtr, "int main() {\n");
+
+	if (verbose) printf("program header written");
+
+	char line[60];
+	while (fgets(line, 60, inPtr) != NULL) {
+		int found = match(line, "print");
+
+		if (found != -1)
+		{
+			int openBracket = match(line, "\"");
+			char contentString[60];
+
+			strcpy(contentString, &(line[openBracket]));
+			contentString[strlen(contentString) - 1] = '\0';
+
+			fprintf(outPtr, "printf(\"%%s\\n\",");
+			fprintf(outPtr, "%s", contentString);
+			fprintf(outPtr, ");\n");
+		}
+		else
+		{
+			printf("compilation error");
+			return;
+		}
+	}
+
+	fprintf(outPtr, "}\n");
 }
 
 int main(int argc, char * argv[]) {
 	int opt;
-	int verbose = 0;
 	char inName[50];
 	char outName[50];
 
 	int inputsOK = 0;
 
-	while((opt = getopt(argc, argv, "vhHtw:o:")) != -1)
+	while((opt = getopt(argc, argv, "vhHw:o:")) != -1)
 	{
 		switch(opt)
 		{
 			case 'h':
 			case 'H':
 				printHelp();
-				return 1;
+				return 0;
 			case 'v':
 				verbose = 1;
 				printf("verbose mode on\n");
 				break;
-			case 't':
+			case 'w':
+				if (verbose) printf("filename: ");
 				strcpy(inName, optarg);
-				if (verbose) printf("filename: %s\n", inName);
+				if (verbose) printf("%s\n", inName);
 				inputsOK |= 1;
 				break;
 			case 'o':
+				if (verbose) printf("filename: ");
 				strcpy(outName, optarg);
-				if (verbose) printf("filename: %s\n", outName);
+				if (verbose) printf("%s\n", outName);
 				inputsOK |= 2;
 				break;
 		}
@@ -57,7 +135,8 @@ int main(int argc, char * argv[]) {
 	{
 		if (inputsOK && 1 == 0) printf("input filename missing\n");
 		if (inputsOK && 2 == 0) printf("output filename missing\n");
+		return 1;
 	}
 
-	return 1;
+	return 0;
 }
